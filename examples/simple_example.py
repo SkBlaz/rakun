e713a1ca0960e81b64f2b7a57578843ea6c2f79a
skblaz <skrljblaz@gmail.com>
2019-09-30 20:14:35 +0200

    added refurbished samples

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -1,6 +1,11 @@
 from mrakun import RakunDetector
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
+from os import path
+
+if not path.exists("../pretrained_models/fasttext/wiki.en.bin"):
+    print("Please, load a fasttext english pretrained model binary into folder ../pretrained_models.")
+    
 
 hyperparameters = {"distance_threshold":0.2,
                    "distance_method": "fasttext",
